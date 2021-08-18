7c21ba46aac7d793c2cbc44a34f1d3a98f5c5826
SaiShashank-zelar <shashank@zelarsoft.com>
2021-08-18 15:33:17 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -4,35 +4,39 @@ provider "aws" {
   region     = "us-east-1"
 }
 resource "aws_instance" "example" {
+  count         = var.instance_count
   ami           = "ami-0eb5f3f64b10d3e0e"
   instance_type = "t2.micro"
-  security_groups = ["${launch-wizard-3}"]
+  security_groups = ["${aws_security_group.ubuntu_allow_http_ssh.name}"]
   tags = {
-    Name = "terra"
+    Name = "terra-${count.index + 1}"
   }
 }
-#resource "aws_security_group" "launch-wizard-3" {
-#  name = "launch-wizard-3"
-#  description = "launch-wizard-3 created 2021-04-12T12:13:23.019+05:30"
-  #ingress {
-  #  from_port = 80
-  #  protocol = "tcp"
-  #  to_port = 80
-  #  cidr_blocks = ["0.0.0.0/0"]
-  #}
-  #ingress {
-  #  from_port = 22
-  #  protocol = "tcp"
-  #  to_port = 22
-  #  cidr_blocks = ["0.0.0.0/0"]
-  #}
-  #egress {
-  #  from_port = 0
-  #  protocol = "-1"
-  #  to_port = 0
-  #  cidr_blocks = ["0.0.0.0/0"]
-  #}
-#}
+resource "aws_security_group" "ubuntu_allow_http_ssh" {
+  name = "ubuntu_allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+variable "instance_count" {
+  default ="2"
+}
 output "ip" {
   value = "${aws_instance.example.public_ip}"
 }
