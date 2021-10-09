72658a1af5af02dbcc70141d02c21598831ee1ec
SaiShashank-zelar <shashank@zelarsoft.com>
2021-10-09 15:05:54 +0530

    update

---
+++ gcp/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:/Users/hp/Desktop/majestic-nimbus-328507-cce470665f77.json")
+
+  project = "majestic-nimbus-328507"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vpc_network" {
+  name         = "terraform"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
